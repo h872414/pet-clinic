305369d17e1b7f27fc4f743aaf05b1407d20de48
Kapcsándi Tamás <tkapcsandi@combit.hu>
2021-02-02 05:58:45 +0100

    Implement Spring Config. Closes #19

--- pet-clinic-data/src/main/java/hu/kt/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/hu/kt/sfgpetclinic/services/PetService.java
@@ -1,7 +1,9 @@
 package hu.kt.sfgpetclinic.services;
 
 import hu.kt.sfgpetclinic.model.Pet;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface PetService extends CrudService<Pet, Long> {
 
 }
