8ac1b45d6e0e3164e0a2e18dfd79b0708f1c852d
Kapcsándi Tamás <tkapcsandi@combit.hu>
2021-02-04 18:17:06 +0100

    Added Specialities, added to vet. Closes #33

--- pet-clinic-data/src/main/java/hu/kt/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/hu/kt/sfgpetclinic/model/Vet.java
@@ -1,6 +1,12 @@
 package hu.kt.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
 
+    private Set<Speciality> specialities;
 
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
 }
